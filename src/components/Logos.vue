1ee72204b0613adec21c46ce761dd450cdc4292d
rstubryan <restu.bumi_ti21@nusaputra.ac.id>
2023-10-09 22:05:35 +0700

    logos example testing

---
+++ src/components/Logos.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+  msg: String,
+});
+</script>
+
+<template>
+  <body class="bg-teal-100">
+    <nav class="text-center text-3xl bg-teal-400 py-4">
+      <p>Halo dunia</p>
+    </nav>
+
+    <section class="">
+      <p>Ini bagian text</p>
+    </section>
+  </body>
+</template>
